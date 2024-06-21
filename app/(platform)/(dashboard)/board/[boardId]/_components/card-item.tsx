e787c21fd5e4149832ee207ae25705166197dbe4
khesir <khesir2002@gmail.com>
2024-06-21 15:12:42 +0800

    feat: Added Server actions for drag and drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
 interface CardItemProps {
@@ -12,10 +13,17 @@ export const CardItem = ({
     index
 }:CardItemProps) => {
     return(
-        <div 
-            role="button"
-            className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-sm shadow-sm">
-            {data.title}
-        </div>
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+                <div 
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    role="button"
+                    className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-sm shadow-sm">
+                    {data.title}
+                </div>
+            )}
+        </Draggable>
     )
 }
